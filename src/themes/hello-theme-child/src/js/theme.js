1da1a2c2ddbd2568d794b469d53cd87699af0e25
mhonromero <mhon.romero@from.digital>
2022-02-05 04:26:10 +0800

    fix nav color bug

--- src/themes/hello-theme-child/src/js/theme.js
+++ src/themes/hello-theme-child/src/js/theme.js
@@ -618,7 +618,7 @@ jQuery(function ($) {
     
     $('#headerSection .elementor-item,.elementor-sub-item').mouseover(function() {
       var  offset = $('#headerSection').data('scroll')
-      if(offset === 0){
+      if(offset > -60 && offset <= 0){
         $('div.page-content').addClass('offsetTop')
       }
 
@@ -630,7 +630,7 @@ jQuery(function ($) {
     })
     .mouseout(function() {
       var  offset = $('#headerSection').data('scroll')
-      if(offset === 0){
+      if(offset > -60 && offset <= 0){
         $('div.page-content').removeClass('offsetTop')
         $('#headerSection .elementor-item').removeClass('hovered')
         $('#contactUs').removeClass('contactUs')
