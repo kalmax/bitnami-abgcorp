db96b486f56a1e0525b08221a4516f0ba8e66f3c
Lucia Ionica <lucia.ionica@from.digital>
2022-03-24 14:43:11 +0200

    Footer color variations - ABGCSI-113

--- src/themes/hello-theme-child/src/js/theme.js
+++ src/themes/hello-theme-child/src/js/theme.js
@@ -747,15 +747,19 @@ jQuery(function ($) {
 
   /** SET Footer container background color */
   /** ESG pages (with subpages?) must have green footer */
+  $('.elementor-location-footer').removeClass('grey');
+  $('.elementor-location-footer').removeClass('green');
+  $('.elementor-location-footer').removeClass('blue');
+
   if ($('body.page.page-id-5730, body.page.page-child.parent-pageid-5730').length) {
     $('.elementor-location-footer').addClass('green');
   }
   /** Contact US pages must have blue footer */
-  if (jQuery('body.page.page-id-7377').length.length) {
+  if ($('body.page.page-id-7377').length) {
     $('.elementor-location-footer').addClass('blue');
   }
   /** News Article pages must have grey footer */
-  if ($('.post.category-news-article').length) {
+  if ($('body.single.single-post div.post.category-news-article').length) {
     $('.elementor-location-footer').addClass('grey');
   }
   /** END SET Footer container background color*/
