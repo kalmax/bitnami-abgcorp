ddfdd1e07d17dd257c519283cf13fa32fa833dec
Sylvester Molina <sylvester@visionaryinterior.com>
2022-04-21 21:41:49 +0800

    Adding tabs-carousel plugin

---
+++ src/plugins/tabs-carousel/assets/js/from-tabs-carousel.js
@@ -0,0 +1,193 @@
+jQuery(document).ready(function(){
+
+    let selector;
+    let tabContainer;
+    let tablSlickContainer;
+
+    // For the editor page
+    jQuery(document).ready(function () {
+        // Sanity Check
+        if (window.elementorFrontend && 'undefined' !== window.elementorFrontend) {
+
+            elementorFrontend.hooks.addAction('frontend/element_ready/from-tabs-carousel.default', function($scope) {
+              selector = `.elementor-element-${$scope[0].dataset.id}`;
+              tabContainer = window.document.querySelector(`${selector} .from-tabs-carousel-container`);
+              tablSlickContainer = window.document.querySelector(`${selector} .from-tabs-carousel-list`);
+              buildTabs(tabContainer);
+              destroyCarousel(tablSlickContainer);
+              buildSlickCarousel(tablSlickContainer);
+            });
+
+        }
+    });
+
+    // Front end
+    jQuery(window).on('elementor/frontend/init', function () {
+
+        // Sanity Check
+        if (window.elementorFrontend && 'undefined' !== window.elementorFrontend) {
+
+            elementorFrontend.hooks.addAction('frontend/element_ready/from-tabs-carousel.default', function($scope) {
+              selector = `.elementor-element-${$scope[0].dataset.id}`;
+              tabContainer = window.document.querySelector(`${selector} .from-tabs-carousel-container`);
+              tablSlickContainer = window.document.querySelector(`${selector} .from-tabs-carousel-list`);
+              buildTabs(tabContainer);
+              destroyCarousel(tablSlickContainer);
+              buildSlickCarousel(tablSlickContainer);
+            });
+
+        }
+    });
+
+    /**
+     * @description Builds tabs based on posts
+     *
+     * @param {object} $scope
+     * @return void
+     */
+    function buildTabs(el) {
+   
+     if (el && el !== 'undefined') {
+        
+        let tabLink = jQuery(el).find('.from-tabs-carousel-list--item');
+       
+        // open first tab
+        openTab(el, 0);
+
+        tabLink.on('click',function(e){
+
+          e.preventDefault();
+
+          let _this = jQuery(this);
+
+          var tabId = _this.data('slick-index');
+        
+          jQuery(el).find(".slick-slide").each(function(index){
+            jQuery(this).attr('class', jQuery(this).attr('class').replace(' active', '') );
+          });
+          
+          _this.addClass("active");
+          openTab(el, tabId);
+
+        });
+
+      }
+
+    }
+
+    function openTab(el, tabId) {
+      
+      console.log(tabId);
+      jQuery(el).find(".from-tabs-carousel-contents--item").each(function(index){
+        jQuery(this).attr('class', jQuery(this).attr('class').replace(' active', '') );
+      });
+      
+      console.log(jQuery(el).find(`#from-tabs-carousel-content-${tabId}`));
+      jQuery(el).find(`#from-tabs-carousel-content-${tabId}`).addClass("active");
+
+    }
+
+    jQuery(window).resize(function(){
+      destroyCarousel();
+      buildSlickCarousel();
+    });
+
+    function destroyCarousel(el) {     
+      if (jQuery(el).hasClass('slick-initialized')) {       
+        jQuery(el).slick('slickRemove', null, null, true);       
+        jQuery(el).slick('destroy');     
+      }         
+    }
+
+    /**
+     * @description Foreach of the carousel elements initiate a Slick carousel.
+     *
+     * @param {object} $scope
+     * @return void
+     */
+    function buildSlickCarousel(el) {
+   
+     if (el && el !== 'undefined') {
+        
+        let sliderParams = {
+          infinite: true,
+          autoplay: false,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          dots: false,
+          arrows: true,
+          prevArrow:"<div class='elementor-icon slick-arrow-left'><button type='button' class='slick-prev pull-left'><i class='fas fa-chevron-left' aria-hidden='true'></i></button></div>",
+          nextArrow:"<div class='elementor-icon slick-arrow-right'><button type='button' class='slick-next pull-right'><i class='fas fa-chevron-right' aria-hidden='true'></i></button></div>",
+          responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows: false
+              }
+            }
+          ]
+        };
+
+        jQuery(el).not('.slick-initialized').slick(sliderParams); 
+        
+        // jQuery(el).not('.slick-initialized').slick({
+        //   infinite: true,
+        //   autoplay: false,
+        //   slidesToShow: 1,
+        //   slidesToScroll: 1,
+        //   dots: false,
+        //   prevArrow: false,
+        //   nextArrow: false
+        // });
+
+        jQuery(el).on('beforeChange', function(event, slick, currentSlide, nextSlide){
+          jQuery(el).find(".tab-contents-mobile-item").each(function(i){
+            if(!jQuery(this).find(".content").hasClass('collapsed')){
+              jQuery(this).find(".excerpt").css({ "display": "block"});
+              jQuery(this).find(".content").addClass('collapsed');
+              jQuery(this).find(".read-more").html("Read More");
+            }
+          });
+        });
+
+        // read more toggle
+        jQuery(el).find(".read-more").click(function(e){
+
+          e.preventDefault();
+
+          if(jQuery(this).parent().find(".content").hasClass("collapsed")) {
+              
+            jQuery(this).parent().find(".excerpt").css({ "display": "none"});
+            jQuery(this).parent().find(".content").removeClass("collapsed");   
+            jQuery(this).html("Read Less");
+
+          } else {
+            
+            jQuery(this).parent().find(".excerpt").css({ "display": "block"});
+            jQuery(this).parent().find(".content").addClass("collapsed");
+            jQuery(this).html("Read More");
+
+            jQuery('html, body').animate({
+              scrollTop: jQuery(el).offset().top
+            }, 1000);
+
+          }    
+
+        });
+
+      }
+
+    }
+
+
+});
---
+++ src/plugins/tabs-carousel/widget.php
@@ -0,0 +1,152 @@
+<?php
+namespace From_Tabs_Carousel;
+
+use Elementor\Plugin;
+use Elementor\Repeater;
+use Elementor\Widget_Base;
+use Elementor\Group_Control_Background;
+use Elementor\Controls_Manager;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class Tabs_Carousel_Widget extends Widget_Base {
+
+  public static $slug = 'from-tabs-carousel';
+
+
+  public function __construct($data = [], $args = null) {
+
+    parent::__construct($data, $args);
+
+    wp_register_style( 'from-tabs-carousel-style', 
+      plugins_url( 'assets/css/from-tabs-carousel.css', __FILE__ ), 
+      array( 
+          'from-carousel-slick', 'from-carousel-slick-theme' 
+      ), 
+      '1.0'
+    );
+    wp_enqueue_style('from-tabs-carousel-style');
+
+   
+    wp_register_script('from-tabs-carousel-js', plugins_url('assets/js/from-tabs-carousel.js', __FILE__), array( 'from-carousel-slick-js' ), '1.0', true);  
+    wp_enqueue_script('from-tabs-carousel-js');
+
+  }
+
+  public function get_name() { return self::$slug; }
+
+  public function get_title() { return __('FROM Tabs Carousel', self::$slug); }
+
+  public function get_icon() { return 'eicon-slider-push'; }
+
+  public function get_categories() { return [ 'general' ]; }
+
+  protected function _register_controls() {
+
+
+    $this->start_controls_section(
+      'content_section',
+      [
+        'label' => __( 'Options', self::$slug ),
+        'tab' => Controls_Manager::TAB_CONTENT,
+      ]
+    );
+
+    $repeater = new \Elementor\Repeater();
+
+    $repeater->add_control(
+      'title', [
+        'label' => esc_html__( 'Title', self::$slug ),
+        'type' => \Elementor\Controls_Manager::TEXT,
+        'default' => esc_html__( 'Title' , self::$slug ),
+        'label_block' => true,
+      ]
+    );
+
+    $repeater->add_control(
+      'image',
+      [
+        'label' => esc_html__( 'Choose Image', self::$slug ),
+        'type' => \Elementor\Controls_Manager::MEDIA,
+        'default' => [
+          'url' => \Elementor\Utils::get_placeholder_image_src(),
+        ],
+      ]
+    );
+
+
+    $repeater->add_control(
+      'content', [
+        'label' => esc_html__( 'Content', self::$slug ),
+        'type' => \Elementor\Controls_Manager::WYSIWYG,
+        'default' => esc_html__( 'Content' , self::$slug ),
+        'show_label' => false,
+      ]
+    );
+
+    $this->add_control(
+      'tabs',
+      [
+        'label' => esc_html__( 'Tabs', self::$slug ),
+        'type' => \Elementor\Controls_Manager::REPEATER,
+        'fields' => $repeater->get_controls(),
+        'title_field' => '{{{ title }}}',
+      ]
+    );
+
+    $this->end_controls_section();
+
+  
+  }
+
+  protected function render() {
+
+    $settings = $this->get_settings_for_display();
+
+    $tabs = $settings['tabs'];
+    // echo '<pre>';
+    // var_dump($settings);
+    // exit();
+    if (Plugin::$instance->editor->is_edit_mode()) {
+      // If the Elementor editor is opened.
+
+    } ?>
+    <div class="wrapper">
+      <div class="from-tabs-carousel from-tabs-carousel-wrapper">
+        <div class="from-tabs-carousel-container">
+          <div class="from-tabs-carousel-list">
+            <?php if($tabs):?>
+            <?php $tabLinkItemId=0;?>
+            <?php foreach ($tabs as $tab): ?>
+              <a href="#" class="from-tabs-carousel-list--item <?php echo $tabLinkItemId === 0 ? 'active' : '';?>">
+                <?= '<img src="' . $tab['image']['url'] . '">';?>
+              </a>
+              <?php $tabLinkItemId++;?>
+            <?php endforeach;?>
+          <?php endif; ?>
+          </div>
+          <div class="from-tabs-carousel-contents">
+            <?php if($tabs):?>
+            <?php $tabItemId=0;?>
+            <?php foreach ($tabs as $tab): ?>
+              <div href="#" class="from-tabs-carousel-contents--item <?php echo $tabItemId === 0 ? 'active' : '';?>" id="from-tabs-carousel-content-<?= $tabItemId;?>">  
+                <?=do_shortcode($tab['content']);?>
+              </div>
+              <?php $tabItemId++;?>
+            <?php endforeach;?>
+            <?php $tabItemId=count($tabs);?>
+            <?php foreach ($tabs as $tab): ?>
+              <div href="#" class="from-tabs-carousel-contents--item <?php echo $tabItemId === 0 ? 'active' : '';?>" id="from-tabs-carousel-content-<?= $tabItemId;?>">  
+                <?=do_shortcode($tab['content']);?>
+              </div>
+              <?php $tabItemId++;?>
+            <?php endforeach;?>
+          <?php endif; ?>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  <?php }
+
+} 
